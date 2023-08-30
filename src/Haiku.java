5d9c1a1b03ec61722793db7b3737fd4060414ae1
30163628 <emerdivefec@outlook.com>
2023-08-30 15:40:50 +0100

    Haiku of quiteness

---
+++ src/Haiku.java
@@ -0,0 +1,10 @@
+public class Haiku {
+    public static void main(String[] args) {
+        String line1 = "Silent tranquility";
+        String line2 = "Peaceful lack of any sound";
+        String line3 = "Calmness soothes the soul";
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+    }
+}
