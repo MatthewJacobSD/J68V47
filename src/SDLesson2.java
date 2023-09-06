24b1e27fb9b8268eca0d35fe89c2306beafb6acc
30163628 <emerdivefec@outlook.com>
2023-09-06 15:23:23 +0100

    Lesson 2

---
+++ src/SDLesson2.java
@@ -0,0 +1,7 @@
+public class SDLesson2 {
+    public static void main(String[] args) {
+    String name = input.nextLine();
+    String hobby = input.nextLine();
+    System.out.println("His is " + name + "and his hobby is" + hobby + ". we")
+    }
+}
